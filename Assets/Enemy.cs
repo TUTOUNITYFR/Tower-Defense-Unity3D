45d8766ddb11b86784e250695b0e909da55a6ac3
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2018-10-12 15:34:50 +0200

    Épisode 2 : Intelligence artificielle des ennemis

---
+++ Assets/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.3f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if(waypointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex];
+    }
+
+}
