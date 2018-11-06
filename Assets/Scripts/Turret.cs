4684816e86fe29c6cf5dd483cb55a9550dc6a1be
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2018-11-06 10:56:04 +0100

    Épisode 5 : Tir de la tourelle

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -11,6 +11,12 @@ public class Turret : MonoBehaviour {
 
     private float turnSpeed = 6.5f;
 
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
 	// Use this for initialization
 	void Start () {
         InvokeRepeating("UpdateTarget", 0f, 0.5f);
@@ -53,7 +59,26 @@ public class Turret : MonoBehaviour {
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if(fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1 / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
 	}
+
+    void Shoot()
+    {
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if(bullet != null)
+        {
+            bullet.Seek(target);
+        }
+    }
 
     private void OnDrawGizmosSelected()
     {
