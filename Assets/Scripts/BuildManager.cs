1e4a28f17f3470e3394d0dd8499a39b52fc1d24b
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2018-11-07 10:06:51 +0100

    Épisode 6 : Système de construction

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    #region Singleton
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("Il y a déjà un BuildManager dans la scène !");
+            return;
+        }
+        instance = this;
+    }
+    #endregion
+
+    public GameObject standardTurretPrefab;
+
+    private void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private GameObject turret;
+    private Color startColor;
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if(turret != null)
+        {
+            Debug.Log("Impossible de construire ici, il y a déjà une tourelle.");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+
+}
