830593b940f0f4470d1550ed484e83d9cd6fa2e5
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2018-11-12 09:24:19 +0100

    Épisode 7 : Gestion de la caméra

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private bool canMove = true;
+
+    public float panSpeed = 30f;
+    public float panBorder = 10f;
+
+    public float scrollSpeed = 5f;
+
+    public float minY = 10f;
+    public float maxY = 80f;
+
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            canMove = !canMove;
+        }
+
+        if (!canMove)
+        {
+            return;
+        }
+
+        // déplacement vers l'avant
+        if (Input.GetKey(KeyCode.Z) || Input.mousePosition.y >= Screen.height - panBorder)
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        // déplacement vers l'arrière
+        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= panBorder)
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        // déplacement vers la gauche
+        if (Input.GetKey(KeyCode.Q) || Input.mousePosition.x <= panBorder)
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        // déplacement vers la droite
+        if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - panBorder)
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 pos = transform.position;
+
+        pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        transform.position = pos;
+    }
+}
