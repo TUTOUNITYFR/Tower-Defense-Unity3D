26423c3dd14e0431b42c06c41e6fcc52af1539e5
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2018-11-26 10:45:16 +0100

    Épisode 10 : Configuration de la tourelle lance-missiles

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,12 +8,13 @@ public class Bullet : MonoBehaviour {
 
     public float speed = 70f;
 
+    public float explosionRadius = 0f;
+
     public void Seek(Transform _target)
     {
         target = _target;
     }
 
-	// Update is called once per frame
 	void Update () {
 		if(target == null)
         {
@@ -31,14 +32,46 @@ public class Bullet : MonoBehaviour {
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
-
+        transform.LookAt(target);
 	}
 
     void HitTarget()
     {
         GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
-        Destroy(effectIns, 2f);
-        Destroy(target.gameObject);
+        Destroy(effectIns, 5f);
+
+        if(explosionRadius > 0)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+
         Destroy(gameObject);
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            if(collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
