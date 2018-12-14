2e2b01f4763a7a5ece1225e7844d7d3573f88681
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2018-12-14 11:08:53 +0100

    Épisode 15 : Particules de la tourelle laser

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -17,6 +17,8 @@ public class Turret : MonoBehaviour {
 
     public bool useLaser;
     public LineRenderer lineRenderer;
+    public ParticleSystem impactEffect;
+    public Light impactLight;
 
     [Header("Unity setup fields")]
 
@@ -66,6 +68,8 @@ public class Turret : MonoBehaviour {
                 if (lineRenderer.enabled)
                 {
                     lineRenderer.enabled = false;
+                    impactEffect.Stop();
+                    impactLight.enabled = false;
                 }
             }
 
@@ -104,10 +108,17 @@ public class Turret : MonoBehaviour {
         if(lineRenderer.enabled == false)
         {
             lineRenderer.enabled = true;
+            impactEffect.Play();
+            impactLight.enabled = true;
         }
 
         lineRenderer.SetPosition(0, firePoint.position);
         lineRenderer.SetPosition(1, target.position);
+
+        Vector3 dir = firePoint.position - target.position;
+        impactEffect.transform.rotation = Quaternion.LookRotation(dir);
+
+        impactEffect.transform.position = target.position + dir.normalized * 1.5f;
     }
 
     void Shoot()
