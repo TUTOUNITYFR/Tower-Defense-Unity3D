87bc9cba6b8fba14b965f58d3d0bb2f9527dbf48
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2019-01-23 10:13:32 +0100

    Épisode 22 : Menu principal

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string levelToLoad = "MainScene";
+
+    public void Play()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Fermeture du jeu");
+        Application.Quit();
+    }
+
+}
