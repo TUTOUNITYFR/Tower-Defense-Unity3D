d98a5c1fe920769c6cd7f577212ee268f39327de
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2019-01-25 09:10:32 +0100

    Épisode 23 : Barre de vie des ennemis

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,23 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
 
     public float startSpeed = 10;
     [HideInInspector]
     public float speed;
-    public float health = 100f;
+    public float startHealth = 100f;
+    private float health;
     public int worth = 50;
 
     public GameObject deathEffect;
 
+    public Image healthbar;
+
     public void Start()
     {
         speed = startSpeed;
+        health = startHealth;
     }
 
     public void TakeDammage(float amount)
     {
         health -= amount;
+
+        healthbar.fillAmount = health / startHealth;
 
         if(health <= 0)
         {
